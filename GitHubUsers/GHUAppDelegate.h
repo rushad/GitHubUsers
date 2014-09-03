2bbc16dd0e2eef8e75afb252e8f8cccf9abebaa7
Rushad Nabiullin <rushad@epbyminw2052t1.minsk.epam.com>
2014-09-03 11:24:58 +0400

    Initial Commit

---
+++ GitHubUsers/GHUAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  GHUAppDelegate.h
+//  GitHubUsers
+//
+//  Created by Rushad Nabiullin on 9/3/14.
+//  Copyright (c) 2014 rushad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GHUAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitHubUsers/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GitHubUsers
+//
+//  Created by Rushad Nabiullin on 9/3/14.
+//  Copyright (c) 2014 rushad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "GHUAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([GHUAppDelegate class]));
+    }
+}
